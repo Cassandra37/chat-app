05a89625134a0720569a1c8e1e71b0f268a96ea3
cassandra37 <avenelcassandra@outlook.com>
2019-10-31 10:19:49 +0100

    modifed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
 import Contact from './components/Contact.js';
-import ContactList from './components/ContactList';
+// import ContactList from './components/ContactList';
 
 function App() {
   return (
     <div className="App">
-      <div>
+      {/* <div>
         <ContactList />
-      </div>
+      </div> */}
           <Contact avatar="https://randomuser.me/api/portraits/women/17.jpg" 
-          name="Anita Sutton" status="Online" online="status-online" />
+          name="Anita Sutton" status="Online" online = "true"/>
 
-      <div>
           <Contact avatar="https://randomuser.me/api/portraits/men/88.jpg"
-          name="Jim George" status="Offline" offline="status-offline"/>
-      </div>
+          name="Jim George" status="Offline" />
 
-      <div>
           <Contact avatar="https://randomuser.me/api/portraits/women/76.jpg"
-          name="Charlotte Ryan" status="Online" online="status-online"/>
-      </div>
+          name="Charlotte Ryan" status="Online" online = "true"/>
 
     </div>    
   );
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import './chat.css';
 
+
 function Contact(props){
     return(
         <div className="Contact">
-                <img className="avatar" src={props.avatar}></img>
+                <img className="avatar" src={props.avatar}/>
+                <div>
+                    <div className="name">
+                        <p className="name">{props.name}</p>    
+                    </div>
+                    
                     <div className="status">
-                        <p className="name">{props.name}</p> 
-                        <i className={props.offline}></i>
-                        <i className={props.online}></i>
+                        {props.online ?  <i className="status-online"></i> :  <i className="status-offline"></i> }
+                        {props.offline ? true : false}
                         <i className="status-text">{props.status}</i>
                     </div>
+                </div>
         </div>   
     );
 }
